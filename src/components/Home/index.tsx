bf0673c5f3518ae5d3c0062243c829c721352c53
acm-97 <alejandro97.acm@gmail.com>
2022-12-25 12:22:47 +0100

    feat: add gradient text

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,6 +2,10 @@ import { memo } from 'react';
 
 // type ComponentProps = {};
 
-const Home = () => <div className="min-h-[100vh] px-14">Home Loaded</div>;
+const Home = () => (
+  <div className=" min-h-[100vh] px-14">
+    <h1 className="gradient-text text-3xl font-black">Home Loaded</h1>
+  </div>
+);
 
 export default memo(Home);
