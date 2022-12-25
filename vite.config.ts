20cf4aceba3bc2c6319c5cb2cb2ba72d54742617
acm-97 <alejandro97.acm@gmail.com>
2022-12-25 07:47:11 +0100

    feat: add and remove configurations
    add tailwindcss
    add daisyui
    add eslint
    add prettier
    update css files
    update tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/*
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+*/
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
