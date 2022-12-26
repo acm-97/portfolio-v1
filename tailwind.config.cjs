0228038eac1e52d75d0ea63a28fddf1e7d001186
acm-97 <alejandro97.acm@gmail.com>
2022-12-26 05:52:01 +0100

    refactor: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -6,13 +8,13 @@ module.exports = {
   },
   plugins: [
     require('daisyui'),
-    plugin(function ({ addBase, theme, addVariant  }) {
+    plugin(function ({ addBase, theme, addVariant }) {
       addBase({
         h1: { fontSize: theme('fontSize.2xl') },
         h2: { fontSize: theme('fontSize.xl') },
         h3: { fontSize: theme('fontSize.lg') },
       });
-      addVariant('hocus', ['&:hover', '&:focus'])
+      addVariant('hocus', ['&:hover', '&:focus']);
     }),
   ],
 };
