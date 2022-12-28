d98b98bbe6c6b5e9b424f56d01f0a0a98f4711e8
acm-97 <alejandro97.acm@gmail.com>
2022-12-28 00:31:52 -0500

    feat: add env vars

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,5 @@
 export { routes } from './navbar';
 export type { routesProps } from './navbar';
+
+export const CRAI = import.meta.env.VITE_CRAI;
+export const DOFLEINI = import.meta.env.VITE_DOFLEINI;
